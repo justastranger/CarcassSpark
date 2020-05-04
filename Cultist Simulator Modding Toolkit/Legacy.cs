7660562a9ebb810f21baf08b10ae9b20d7f2578e
justastranger <justastrangerlica@gmail.com>
2020-05-03 19:32:13 -0500

    Legacy Support

---
+++ Cultist Simulator Modding Toolkit/Legacy.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultist_Simulator_Modding_Toolkit
+{
+    public class Legacy
+    {
+        public string id, label, description, startdescription, image, fromEnding, startingVerbId;
+        public string[] excludesOnEnding;
+        public ElementDictionary effects;
+        public bool availableWithoutEndingMatch;
+
+        [JsonConstructor]
+        public Legacy(string id, string label, string description, string startdescription,
+                      JToken effects, string image, string fromEnding, bool availableWithoutEndingMatch,
+                      string startingVerbId = null, JArray excludesOnEnding = null)
+        {
+            this.id = id;
+            this.label = label;
+            this.description = description;
+            this.startdescription = startdescription;
+            this.effects = effects.ToObject<ElementDictionary>();
+            this.image = image;
+            this.fromEnding = fromEnding;
+            this.availableWithoutEndingMatch = availableWithoutEndingMatch;
+            this.startingVerbId = startingVerbId;
+            if (excludesOnEnding != null) this.excludesOnEnding = excludesOnEnding.ToObject<string[]>();
+        }
+    }
+}
