bb0f3d5896d0279efacbe3479c1df208ae5b5b39
justastranger <justastrangerlica@gmail.com>
2020-09-01 08:09:34 -0500

    Moved stuff around the settings menu and added tooltips

--- CarcassSpark/Settings.Designer.cs
+++ CarcassSpark/Settings.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
             this.loadPreviousModsCheckBox = new System.Windows.Forms.CheckBox();
             this.previousModsTextBox = new System.Windows.Forms.TextBox();
@@ -39,33 +40,38 @@
             this.GamePathTextBox = new System.Windows.Forms.TextBox();
             this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ToolTip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // loadPreviousModsCheckBox
             // 
             this.loadPreviousModsCheckBox.AutoSize = true;
-            this.loadPreviousModsCheckBox.Location = new System.Drawing.Point(12, 81);
+            this.loadPreviousModsCheckBox.Location = new System.Drawing.Point(12, 35);
             this.loadPreviousModsCheckBox.Name = "loadPreviousModsCheckBox";
             this.loadPreviousModsCheckBox.Size = new System.Drawing.Size(218, 17);
             this.loadPreviousModsCheckBox.TabIndex = 1;
             this.loadPreviousModsCheckBox.Text = "Load Remembered Mods When Opened";
+            this.ToolTip.SetToolTip(this.loadPreviousModsCheckBox, "When true, all mods that were open when you closed Carcass Spark will be loaded w" +
+        "hen Carcass Spark opens.\r\nBelow are paths to the mods\' directories.");
             this.loadPreviousModsCheckBox.UseVisualStyleBackColor = true;
             this.loadPreviousModsCheckBox.CheckedChanged += new System.EventHandler(this.LoadPreviousModsCheckBox_CheckedChanged);
             // 
             // previousModsTextBox
             // 
             this.previousModsTextBox.AcceptsReturn = true;
-            this.previousModsTextBox.Location = new System.Drawing.Point(12, 117);
+            this.previousModsTextBox.Location = new System.Drawing.Point(12, 71);
             this.previousModsTextBox.Multiline = true;
             this.previousModsTextBox.Name = "previousModsTextBox";
             this.previousModsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.previousModsTextBox.Size = new System.Drawing.Size(370, 81);
             this.previousModsTextBox.TabIndex = 2;
+            this.ToolTip.SetToolTip(this.previousModsTextBox, resources.GetString("previousModsTextBox.ToolTip"));
             // 
             // previousModLabel
             // 
             this.previousModLabel.AutoSize = true;
-            this.previousModLabel.Location = new System.Drawing.Point(12, 101);
+            this.previousModLabel.Location = new System.Drawing.Point(12, 55);
             this.previousModLabel.Name = "previousModLabel";
             this.previousModLabel.Size = new System.Drawing.Size(123, 13);
             this.previousModLabel.TabIndex = 3;
@@ -96,32 +102,37 @@
             // loadAllFlowchartNodesCheckBox
             // 
             this.loadAllFlowchartNodesCheckBox.AutoSize = true;
-            this.loadAllFlowchartNodesCheckBox.Location = new System.Drawing.Point(12, 58);
+            this.loadAllFlowchartNodesCheckBox.Location = new System.Drawing.Point(12, 12);
             this.loadAllFlowchartNodesCheckBox.Name = "loadAllFlowchartNodesCheckBox";
             this.loadAllFlowchartNodesCheckBox.Size = new System.Drawing.Size(189, 17);
             this.loadAllFlowchartNodesCheckBox.TabIndex = 7;
             this.loadAllFlowchartNodesCheckBox.Text = "Load All Flowchart Nodes At Once";
+            this.ToolTip.SetToolTip(this.loadAllFlowchartNodesCheckBox, resources.GetString("loadAllFlowchartNodesCheckBox.ToolTip"));
             this.loadAllFlowchartNodesCheckBox.UseVisualStyleBackColor = true;
             this.loadAllFlowchartNodesCheckBox.CheckedChanged += new System.EventHandler(this.LoadAllFlowchartNodesCheckBox_CheckedChanged);
             // 
             // portableCheckBox
             // 
             this.portableCheckBox.AutoSize = true;
-            this.portableCheckBox.Location = new System.Drawing.Point(12, 204);
+            this.portableCheckBox.Location = new System.Drawing.Point(12, 158);
             this.portableCheckBox.Name = "portableCheckBox";
             this.portableCheckBox.Size = new System.Drawing.Size(95, 17);
             this.portableCheckBox.TabIndex = 8;
             this.portableCheckBox.Text = "Portable Mode";
+            this.ToolTip.SetToolTip(this.portableCheckBox, "When true, Carcass Spark will look in the below folder for Cultist Simulator\'s JS" +
+        "ON files.");
             this.portableCheckBox.UseVisualStyleBackColor = true;
             this.portableCheckBox.CheckedChanged += new System.EventHandler(this.PortableCheckBox_CheckedChanged);
             // 
             // GamePathTextBox
             // 
-            this.GamePathTextBox.Location = new System.Drawing.Point(12, 227);
+            this.GamePathTextBox.Location = new System.Drawing.Point(12, 194);
             this.GamePathTextBox.Name = "GamePathTextBox";
             this.GamePathTextBox.ReadOnly = true;
             this.GamePathTextBox.Size = new System.Drawing.Size(370, 20);
             this.GamePathTextBox.TabIndex = 9;
+            this.ToolTip.SetToolTip(this.GamePathTextBox, "This is the path to your installation of Cultist Simulator.\r\nIt must point to the" +
+        " folder containing cultistsimulator.exe");
             this.GamePathTextBox.DoubleClick += new System.EventHandler(this.GamePathTextBox_DoubleClick);
             // 
             // folderBrowserDialog
@@ -133,6 +144,15 @@
             this.openFileDialog.FileName = "Assembly-CSharp.dll";
             this.openFileDialog.Filter = "Assembly-CSharp.dll|Assembly-CSharp.dll";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Cultist Simulator Game Path";
+            // 
             // Settings
             // 
             this.AcceptButton = this.okButton;
@@ -140,6 +160,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
             this.ClientSize = new System.Drawing.Size(394, 383);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.GamePathTextBox);
             this.Controls.Add(this.portableCheckBox);
             this.Controls.Add(this.loadAllFlowchartNodesCheckBox);
@@ -169,5 +190,7 @@
         private System.Windows.Forms.TextBox GamePathTextBox;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip ToolTip;
     }
 }
