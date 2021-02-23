19f4ed10399fa1dc4c98505a43f8afd4ba8b45f8
justastranger <justastrangerlica@gmail.com>
2021-02-23 12:30:47 -0600

    Add JsonIgnore annotations to IGameObject properties

--- CarcassSpark/ObjectTypes/IGameObject.cs
+++ CarcassSpark/ObjectTypes/IGameObject.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace CarcassSpark.ObjectTypes
 {
     public interface IGameObject
     {
+        [JsonIgnore]
         Guid Guid { get; set; }
+        [JsonIgnore]
         string ID { get; set; }
+        [JsonIgnore]
         string Filename { get; set; }
     }
 }
